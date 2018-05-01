0788bc15dbd74235b1ce5c04edd78427bb1afa96
MachoDhogo <chriswvliew@gmail.com>
2018-05-01 19:15:33 +0000

    Added add_person.js file

---
+++ add_person.js
@@ -0,0 +1,32 @@
+const settings = require("./settings");
+
+var knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host : settings.hostname,
+    user : settings.user,
+    password : settings.password,
+    database : settings.database
+  }
+});
+
+//Insert as callback
+knex('famous_people').insert({
+  first_name: process.argv[2],
+  last_name: process.argv[3],
+  birthdate: process.argv[4]
+})
+.asCallback(function(err, rows) {
+  if(err) return console.error(err);
+  console.log(rows);
+});
+
+//Select as callback
+knex.select('*').from('famous_people')
+.asCallback(function(err, rows) {
+  if(err) return console.error(err);
+  console.log(rows);
+  knex.destroy();
+});
+
+
